fb39ddf37dc2895da5ffcfd191b2534f0abc33fa
Igor Spasić <igor@jodd.org>
2018-11-19 17:04:48 +0100

    no string

--- src/fns/fetchMeetups.js
+++ src/fns/fetchMeetups.js
@@ -10,7 +10,7 @@ exports.handler = async (event, context) => {
     .then(response => response.json())
     .then(data => ({
       statusCode: 200,
-      body: `${data}`
+      body: data
     }))
     .catch(error => ({ statusCode: 422, body: String(error) }));
 };
