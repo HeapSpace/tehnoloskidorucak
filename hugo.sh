b62a691275b845c46b54a82928086df7aaba3a6c
Stefan Feješ <ns.fejes.stefan@gmail.com>
2018-11-23 16:45:38 +0100

    update hugo.sh to curl all resources

--- hugo.sh
+++ hugo.sh
@@ -9,5 +9,7 @@ include ".env"
 readonly airtable="https://api.airtable.com/v0/appNUbQdF6KjbUOHy"
 
 curl "$airtable/Meetups?api_key=$AIRTABLE_TOKEN" -o data/Meetups.json
+curl "$airtable/Presenters?api_key=$AIRTABLE_TOKEN" -o data/Presenters.json
+curl "$airtable/Locations?api_key=$AIRTABLE_TOKEN" -o data/Locations.json
 
 hugo "$@"
