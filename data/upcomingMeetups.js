161d8dbb487b12e92e10deeb6339dd35520cf90e
Stefan Feješ <ns.fejes.stefan@gmail.com>
2018-11-26 12:06:48 +0100

    add generator, fix typos, generate upcomingMeetups.json

--- data/upcomingMeetups.js
+++ data/upcomingMeetups.js
@@ -1,6 +1,8 @@
-const meetups = require('../../data/Meetups.json');
-const presenters = require('../../data/Presenters.json');
-const locations = require('../../data/Locations.json');
+const fs = require('fs');
+
+const meetups = require('./Meetups.json');
+const presenters = require('./Presenters.json');
+const locations = require('./Locations.json');
 
 // Sorted meetups from newest to oldest;
 const sortedMeetups = meetups.records.sort(function(a, b) {
@@ -30,33 +32,53 @@ const presentersZG = presenters.records.filter(x =>
 // Match locations with regions
 const locationBEG = locations.records.filter(x =>
   x.fields.Meetups.includes(meetupsInBEG[0].id)
-);
+)[0].fields.Name;
 
 const locationNS = locations.records.filter(x =>
   x.fields.Meetups.includes(meetupsInNS[0].id)
-);
+)[0].fields.Name;
 
-const locationsZG = locations.records.filter(x =>
+const locationZG = locations.records.filter(x =>
   x.fields.Meetups.includes(meetupsInZG[0].id)
-);
+)[0].fields.Name;
+
+console.log(locationBEG);
 
 const upcomingMeetups = {
   BEG: {
     date: meetupsInBEG[0].fields.Date,
     url: meetupsInBEG[0].fields.URL,
-    presenters: presentersBEG,
+    presenters: sortAlphabetically(presentersBEG),
     location: locationBEG
   },
   NS: {
     date: meetupsInNS[0].fields.Date,
     url: meetupsInNS[0].fields.URL,
-    presenters: presentersNS,
+    presenters: sortAlphabetically(presentersNS),
     location: locationNS
   },
   ZG: {
     date: meetupsInZG[0].fields.Date,
     url: meetupsInZG[0].fields.URL,
-    presenters: presentersZG,
-    location: locationsZG
+    presenters: sortAlphabetically(presentersZG),
+    location: locationZG
   }
 };
+
+// generate upcomingMeetups.json from formated data
+fs.writeFile(
+  './data/upcomingMeetups.json',
+  JSON.stringify(upcomingMeetups),
+  function(err) {
+    if (err) return console.log(err);
+    console.log('Successfully generated upcomingMeetups.json file');
+  }
+);
+
+function sortAlphabetically(array) {
+  return array.sort(function(a, b) {
+    b = b.fields.Name.toUpperCase();
+    a = a.fields.Name.toUpperCase();
+    return a > b ? 1 : a < b ? -1 : 0;
+  });
+}
