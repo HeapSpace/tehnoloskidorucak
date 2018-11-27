1af344e47501af0a6e34b6f9689594572ac14b96
Igor Spasić <igor@jodd.org>
2018-11-27 17:14:20 +0100

    fetchData

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 const fs = require('fs');
 
-const upcomingMeetupsName = 'UpcomingMeetups';
-const upcomingMeetups = require('./scripts/generate' + upcomingMeetupsName);
+const fd = require('./scripts/fetchData');
 
-fs.writeFile(
-  `./data/${upcomingMeetupsName}.json`,
-  JSON.stringify(upcomingMeetups),
-  function(err) {
-    if (err) return console.log(err);
-    console.log(`Successfully generated: ${upcomingMeetupsName}.json`);
-  }
-);
+fd.fetch('Presenters')
+	.then(function() {return fd.fetch('Meetups');})
+	.then(function() {return fd.fetch('Locations');})
+	.then(function() {
+		console.log("Fetching AirTables done.");
+
+		const upcomingMeetupsName = 'UpcomingMeetups';
+		const upcomingMeetups = require('./scripts/generate' + upcomingMeetupsName);
+
+		fs.writeFile(
+		  `./data/${upcomingMeetupsName}.json`,
+		  JSON.stringify(upcomingMeetups),
+		  function(err) {
+		    if (err) return console.log(err);
+		    console.log(`Successfully generated: ${upcomingMeetupsName}.json`);
+		  }
+		);
+	})
+	.catch(function(err) {
+		console.log(err);
+	});
