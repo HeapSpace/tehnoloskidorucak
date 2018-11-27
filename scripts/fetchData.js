4d4d0c8a24777e3259ffa1db39790398f56b2bf0
Nikola Đuza <nikola.duza@toptal.com>
2018-11-27 19:43:18 +0100

    Move fetchNextPage callback into writeFile callback

--- scripts/fetchData.js
+++ scripts/fetchData.js
@@ -14,38 +14,37 @@ function fetch(name) {
     base(name).select({
       maxRecords: 1000
     })
-    .eachPage(
-      function page(records, fetchNextPage) {
-        records.map(record => {
-          // flatten object
-          temporaryData.push({
-            id: record._rawJson.id,
-            ...record._rawJson.fields
+      .eachPage(
+        function page(records, fetchNextPage) {
+          records.map(record => {
+            // flatten object
+            temporaryData.push({
+              id: record._rawJson.id,
+              ...record._rawJson.fields
+            });
           });
-        });
 
-        fs.writeFileSync(
-          `./data/${name}.json`,
-          JSON.stringify(temporaryData),
-          function(err) {
-            if (err) return console.log(err);
+          fs.writeFile(
+            `./data/${name}.json`,
+            JSON.stringify(temporaryData),
+            err => {
+              if (err) return console.log(err);
+              // To fetch the next page of records, call `fetchNextPage`.
+              // If there are more records, `page` will get called again.
+              // If there are no more records, `done` will get called.
+              fetchNextPage();
+            }
+          );
+        },
+        function done(err) {
+          if (err) {
+            console.error(err);
+            reject(err);
+            return;
           }
-        );
-
-        // To fetch the next page of records, call `fetchNextPage`.
-        // If there are more records, `page` will get called again.
-        // If there are no more records, `done` will get called.
-        fetchNextPage();
-      },
-      function done(err) {
-        if (err) {
-          console.error(err);
-          reject(err);
-          return;
+          resolve();
+          console.log(`Successfully fetched: ${name}.json`);
         }
-        resolve();
-        console.log(`Successfully fetched: ${name}.json`);
-      }
-    );
+      );
   });
 }
