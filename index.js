dd49212c48d15df6c6b4c90043382ba173329457
Nikola Đuza <nikola.duza@toptal.com>
2018-11-27 19:37:06 +0100

    Fix indentation in index.js

--- index.js
+++ index.js
@@ -6,18 +6,18 @@ fd.fetch('Presenters')
   .then(() => fd.fetch('Meetups'))
   .then(() => fd.fetch('Locations'))
   .then(() => {
-		console.log("Fetching AirTables done.");
+    console.log("Fetching AirTables done.");
 
-		const upcomingMeetupsName = 'UpcomingMeetups';
-		const upcomingMeetups = require('./scripts/generate' + upcomingMeetupsName);
+    const upcomingMeetupsName = 'UpcomingMeetups';
+    const upcomingMeetups = require('./scripts/generate' + upcomingMeetupsName);
 
-		fs.writeFile(
-		  `./data/${upcomingMeetupsName}.json`,
-		  JSON.stringify(upcomingMeetups),
+    fs.writeFile(
+      `./data/${upcomingMeetupsName}.json`,
+      JSON.stringify(upcomingMeetups),
       err => {
-		    if (err) return console.log(err);
-		    console.log(`Successfully generated: ${upcomingMeetupsName}.json`);
-		  }
-		);
-	})
+        if (err) return console.log(err);
+        console.log(`Successfully generated: ${upcomingMeetupsName}.json`);
+      }
+    );
+  })
   .catch(err => console.log(err));
